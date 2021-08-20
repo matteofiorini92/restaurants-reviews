c215499ee477d0529dc8e097b16d42e12c948953
matteofiorini92 <matteo.fiorini@rocketmail.com>
2021-08-20 08:34:52 +0000

    feat: added buttons to add restaurant form. Tested: form created new document in restaurants collection of db

--- app.py
+++ app.py
@@ -21,9 +21,30 @@ def get_restaurants():
     return render_template("restaurants.html", restaurants=restaurants)
 
 
-@app.route("/add_restaurant")
+@app.route("/add_restaurant", methods=["GET", "POST"])
 def add_restaurant():
-    return render_template("add_restaurant.html")
+    if request.method == "POST":
+        has_vegan_options = True if request.form.get("has_vegan_options") else False
+        has_gluten_free_options = True if request.form.get("has_gluten_free_options") else False
+        restaurant = {
+            "restaurant_name": request.form.get("restaurant_name"),
+            "address_street": request.form.get("address_street"),
+            "address_city": request.form.get("address_city"),
+            "address_county": request.form.get("address_county"),
+            "tel": request.form.get("tel"),
+            "email_address": request.form.get("email_address"),
+            "website": request.form.get("website"),
+            "fb_page": request.form.get("fb_page"),
+            "ig_page": request.form.get("ig_page"),
+            "has_vegan_options": has_vegan_options,
+            "has_gluten_free_options": has_gluten_free_options,
+            "pricing_score": request.form.get("pricing_score"),
+            "created_by": "mfiorini",
+            "status": "pending"
+        }
+        mongo.db.restaurants.insert_one(restaurant)
+    counties = mongo.db.counties.find().sort("name", 1)
+    return render_template("add_restaurant.html", counties=counties)
 
 
 if __name__ == "__main__":
