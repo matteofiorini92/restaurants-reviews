2e7c0c8b2fb702ca461fdb643acaece6c193e3ca
matteofiorini92 <matteo.fiorini@rocketmail.com>
2021-08-24 22:15:18 +0000

    docs: added comment to app.py file, made it pep8 compliant

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ if os.path.exists("env.py"):
     import env
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-from flask import Flask, flash, render_template, redirect, request, session, url_for
+from flask import Flask, flash, render_template, \
+    redirect, request, session, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
@@ -20,18 +21,37 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/get_restaurants")
 def get_restaurants():
+    """
+    Renders the main page with the list of restaurants
+    If the user is logged in, the username is passed
+    as a parameter as buttons such as edit_review are only available
+    if the current user is the author of the review
+    """
     if session:
         user = mongo.db.users.find_one({"username": session["user"]})
     else:
         user = [{"role": ""}, {"username": ""}]
     restaurants = mongo.db.restaurants.find({"status": "approved"})
     counties = mongo.db.counties.find().sort("name", 1)
-    return render_template("get_restaurants.html", restaurants=restaurants, user=user, counties=counties)
+    return render_template(
+        "get_restaurants.html",
+        restaurants=restaurants,
+        user=user,
+        counties=counties
+    )
 
 
 @app.route("/add_restaurant", methods=["GET", "POST"])
 def add_restaurant():
+    """
+    Renders the page to add a new restaurant
+    Only available to registered and logged in users
+    Allows post method that adds the new restaurant to the DB
+    The list of counties is passed as a parameter to build
+    the dropdown menu using jinja2
+    """
     if not session:
+        # 401 = unauthorized - triggered if the user is not logged in
         return render_template("401.html")
     else:
         if request.method == "POST":
@@ -43,7 +63,7 @@ def add_restaurant():
                 has_gluten_free_options = True
             else:
                 has_gluten_free_options = False
-            restaurant = {
+            restaurant = {  # build the new restaurant with info from form
                 "name": request.form.get("name"),
                 "address_street": request.form.get("address_street"),
                 "address_city": request.form.get("address_city"),
@@ -62,48 +82,58 @@ def add_restaurant():
                 "reviews": []
             }
             mongo.db.restaurants.insert_one(restaurant)
+            # add restaurant to the DB
         counties = mongo.db.counties.find().sort("name", 1)
         return render_template("add_restaurant.html", counties=counties)
 
 
 @app.route("/login_register", methods=["GET", "POST"])
 def login_register():
+    """
+    One function to either login or register
+    This is determined based on "type" hidden field of the form
+    If attempting to register, checks if user already exists
+    The authentication part is taken from
+    https://github.com/matteofiorini92/TaskManagerAuth/
+    blob/main/08-SearchingWithinTheDatabase/01-text_index_searching/app.py
+    """
     if request.method == "POST":
-        type = request.form.get("action")
+        type = request.form.get("action")  # either "register" or "login"
         if type == "register":
-            # check if username already
+            # check if username already exists
             existing_user = mongo.db.users.find_one(
                 {"username": request.form.get("username").lower()})
-
             if existing_user:
+                # if user already exists - error
                 flash("Username already exists")
                 return redirect(url_for("login_register"))
-
-            register = {
-                "username": request.form.get("username").lower(),
-                "password": generate_password_hash(request.
-                                                   form.get("password")),
-                "address_county": request.form.get("address_county").lower(),
-                "role": "user"
-            }
-            mongo.db.users.insert_one(register)
-
+            else:
+                # if user doesn't exist, create new one
+                register = {
+                    "username": request.form.get("username").lower(),
+                    "password": generate_password_hash(
+                        request.form.get("password")
+                    ),
+                    "address_county": request.form.get("address_county").lower(),
+                    "role": "user"
+                }
+                # user added to the DB
+                mongo.db.users.insert_one(register)
             # put the new user into 'session' cookie
             session["user"] = request.form.get("username").lower()
             flash("Registration Successful!")
             return redirect(url_for("get_restaurants",
                                     username=session["user"]))
-
-        elif type == "log_in":
-            # check if username exists in db
+        else:  # type is log_in
             existing_user = mongo.db.users.find_one({"username":
                                                     request.form.
                                                     get("username").lower()})
-
+            # check if username exists in db
             if existing_user:
-                # ensure hashed password matches user input
-                if check_password_hash(existing_user["password"],
-                                       request.form.get("password")):
+                if check_password_hash(
+                    existing_user["password"],
+                    request.form.get("password")
+                ):
                     session["user"] = request.form.get("username").lower()
                     flash("Welcome, {}".format(request.form.get("username")))
                     return redirect(url_for("get_restaurants",
@@ -112,73 +142,121 @@ def login_register():
                     # invalid password match
                     flash("Incorrect Username and/or Password")
                     return redirect(url_for("login_register"))
-
             else:
                 # username doesn't exist
                 flash("Incorrect Username and/or Password")
                 return redirect(url_for("login_register"))
-
     counties = mongo.db.counties.find().sort("name", 1)
     return render_template("login_register.html", counties=counties)
 
 
-""" https://stackoverflow.com/questions/14032066/can-flask-have-optional-url-parameters """
-
-
-@app.route("/add_review", defaults={'restaurant_id': None}, methods=["GET", "POST"])
+# The following is taken from
+# https://stackoverflow.com/questions/14032066/can-flask-have-optional-url-parameters
+@app.route("/add_review", defaults={'restaurant_id': None},
+           methods=["GET", "POST"])
 @app.route("/add_review/<restaurant_id>", methods=["GET", "POST"])
 def add_review(restaurant_id):
+    """
+    Function to add a new review to a restaurant.
+    restaurant_id id optional as the user can get to the
+    add_review page from two paths:
+    1. Use the navbar: no restaurant_id is passed in this case
+    2. go from get_restaurants: if a restaurant has 0 reviews,
+    there is the option to leave the first one.
+    In this case, the restaurant_id is passed so that the restaurant dropdown
+    menu is already set to the restaurant the user was looking at
+    """
     if not session:
+        # 401 = unauthorized - triggered if the user is not logged in
         return render_template("401.html")
-    else:
-        restaurants = mongo.db.restaurants.find({"status": "approved"}).sort(
-                                                "name", 1)
-        if restaurant_id:
-            restaurant = mongo.db.restaurants.find_one({"_id": ObjectId(restaurant_id)}, {"name": 1, "_id": 0})["name"]
+    else:  # get list of restaurants from DB to pass as a parameter
+        restaurants = mongo.db.restaurants.find(
+            {"status": "approved"}
+        ).sort(
+            "name", 1
+        )
+        if restaurant_id:  # if restaurant_id exists, find the relevant name
+            restaurant = mongo.db.restaurants.find_one(
+                {"_id": ObjectId(restaurant_id)},
+                {"name": 1, "_id": 0}
+            )["name"]
         else:
             restaurant = ""
         if request.method == "POST":
             name = request.form.get("name")
-            review = {
+            review = {  # create review to add to the DB
                 "_id": ObjectId(),
                 "author": session["user"],
                 "description": request.form.get("description"),
                 "star_score": int(request.form.get("star_score"))
             }
-            mongo.db.restaurants.update_one({"name": name},
-                                            {"$push": {"reviews": review}})
-            mongo.db.restaurants.update_one({"name": name}, {"$set": {"avg_star_score": calculate_average_star_score(name)}})
-        return render_template("add_review.html", restaurants=restaurants, chosen_restaurant=restaurant)
+            mongo.db.restaurants.update_one({  # add review to DB
+                "name": name}, {
+                    "$push": {
+                        "reviews": review
+                    }
+                }
+            )
+            mongo.db.restaurants.update_one({  # calculate new avg star score
+                "name": name}, {
+                    "$set": {
+                        "avg_star_score": calculate_average_star_score(name)
+                    }
+                }
+            )
+        return render_template(
+            "add_review.html",
+            restaurants=restaurants,
+            chosen_restaurant=restaurant
+        )
 
 
 @app.route("/logout")
 def logout():
+    """
+    Logs out the user
+    """
     session.pop("user")
     return redirect(url_for('login_register'))
 
 
 @app.route("/approve_restaurants", methods=["GET", "POST"])
 def approve_restaurants():
+    """
+    This function is only available to admin users
+    It allows to change the status of a restaurant from
+    "pending" to "approved". Only admin approved restaurants
+    can have reviews and are displayed in the get_restaurants page
+    """
     if not session:
+        # 401 = unauthorized - triggered if the user is not logged in
         return render_template("401.html")
     else:
         user = mongo.db.users.find_one({"username": session["user"]})
         if user["role"] != "admin":
+            # 403 = forbidden - triggered if the user is not an admin
             return render_template("403.html")
         else:
             restaurants = mongo.db.restaurants.find({"status": "pending"})
             if request.method == "POST":
                 name = request.form.get("name")
-                print(name)
-                mongo.db.restaurants.update_one({"name": name},
-                                                {"$set": {"status": "approved"}})
-            return render_template("approve_restaurants.html",
-                                restaurants=restaurants)
+                mongo.db.restaurants.update_one({
+                    "name": name}, {
+                        "$set": {
+                            "status": "approved"
+                        }
+                    }
+                )
+            return render_template(
+                "approve_restaurants.html",
+                restaurants=restaurants
+            )
 
 
 @app.route("/my_reviews")
 def my_reviews():
     if not session:
+        # 401 = unauthorized - triggered if the user is not logged in
         return render_template("401.html")
     else:
         user = session["user"]
@@ -187,25 +265,42 @@ def my_reviews():
         for restaurant in restaurants:
             for review in restaurant["reviews"]:
                 if review["author"] == user:
-                    reviews.append({"name": restaurant["name"], "review": review})
+                    reviews.append(
+                        {"name": restaurant["name"],
+                         "review": review})
         return render_template("my_reviews.html", reviews=reviews)
 
 
 @app.route("/edit_review/<review_id>", methods=["GET", "POST"])
 def edit_review(review_id):
-    restaurants = mongo.db.restaurants.find({"reviews._id": ObjectId(review_id)})
+    restaurants = mongo.db.restaurants.find(
+        {"reviews._id": ObjectId(review_id)})
     for restaurant in restaurants:
         reviews = restaurant["reviews"]
         for review in reviews:
             if review["_id"] == ObjectId(review_id):
                 break
     if request.method == "POST":
-        query = {"name": restaurant["name"], "reviews._id": ObjectId(review_id)}
-        update_description = {"$set": {"reviews.$.description": request.form.get("description")}}
-        update_star_score = {"$set": {"reviews.$.star_score": int(request.form.get("star_score"))}}
+        query = {
+            "name": restaurant["name"],
+            "reviews._id": ObjectId(review_id)}
+        update_description = {
+            "$set": {
+                "reviews.$.description": request.form.get("description")}}
+        update_star_score = {
+            "$set": {
+                "reviews.$.star_score": int(request.form.get("star_score"))}}
         mongo.db.restaurants.update_one(query, update_description)
         mongo.db.restaurants.update_one(query, update_star_score)
-        mongo.db.restaurants.update_one({"name": restaurant["name"]}, {"$set": {"avg_star_score": calculate_average_star_score(restaurant["name"])}})
+        mongo.db.restaurants.update_one({
+            "name": restaurant["name"]}, {
+                "$set": {
+                    "avg_star_score": calculate_average_star_score(
+                        restaurant["name"]
+                    )
+                }
+            }
+        )
         return render_template("my_reviews.html")
     return render_template("edit_review.html",
                            review=review, restaurant=restaurant)
@@ -214,13 +309,17 @@ def edit_review(review_id):
 @app.route("/edit_restaurant/<restaurant_id>", methods=["GET", "POST"])
 def edit_restaurant(restaurant_id):
     if not session:
+        # 401 = unauthorized - triggered if the user is not logged in
         return render_template("401.html")
     else:
         user = mongo.db.users.find_one({"username": session["user"]})
         if user["role"] != "admin":
+            # 403 = forbidden - triggered if the user is not an admin
             return render_template("403.html")
         else:
-            restaurant = mongo.db.restaurants.find_one({"_id": ObjectId(restaurant_id)})
+            restaurant = mongo.db.restaurants.find_one(
+                {"_id": ObjectId(restaurant_id)}
+            )
             counties = mongo.db.counties.find().sort("name", 1)
             if request.method == "POST":
                 if request.form.get("has_vegan_options"):
@@ -249,24 +348,44 @@ def edit_restaurant(restaurant_id):
                 }}
                 mongo.db.restaurants.update_one(query, update_restaurant)
                 return redirect(url_for("get_restaurants"))
-            return render_template("edit_restaurant.html",
-                                restaurant=restaurant, counties=counties)
+            return render_template(
+                "edit_restaurant.html",
+                restaurant=restaurant,
+                counties=counties
+            )
 
 
-@app.route("/delete_restaurant/<restaurant_id>", methods=["GET", "POST"])
+@app.route("/delete_restaurant/<restaurant_id>")
 def delete_restaurant(restaurant_id):
     mongo.db.restaurants.delete_one({"_id": ObjectId(restaurant_id)})
     return redirect(url_for("get_restaurants"))
 
 
-@app.route("/delete_review/<review_id>", methods=["GET", "POST"])
+@app.route("/delete_review/<review_id>")
 def delete_review(review_id):
-    restaurant = mongo.db.restaurants.find_one({"reviews._id": ObjectId(review_id)})
+    restaurant = mongo.db.restaurants.find_one(
+        {"reviews._id": ObjectId(review_id)}
+    )
     """ https://stackoverflow.com/questions/15641492/mongodb-remove-object-from-array """
-    mongo.db.restaurants.update({"_id": ObjectId(restaurant["_id"])},
-    {"$pull": {"reviews": {"_id": ObjectId(review_id)}}})
-    mongo.db.restaurants.update_one({"name": restaurant["name"]}, {"$set": {"avg_star_score": calculate_average_star_score(restaurant["name"])}})
-    return redirect(url_for("get_restaurants"))
+    mongo.db.restaurants.update({
+        "_id": ObjectId(restaurant["_id"])}, {
+            "$pull": {
+                "reviews": {
+                    "_id": ObjectId(review_id)
+                }
+            }
+        }
+    )
+    mongo.db.restaurants.update_one({
+        "name": restaurant["name"]}, {
+            "$set": {
+                "avg_star_score": calculate_average_star_score(
+                    restaurant["name"]
+                )
+            }
+        }
+    )
+    return redirect(url_for("my_reviews"))
 
 
 @app.route("/search", methods=["GET", "POST"])
@@ -283,7 +402,13 @@ def search():
             all = list(mongo.db.restaurants.find({"$text": {"$search": name}}))
         else:
             all = []
-            by_name = list(mongo.db.restaurants.find({"$text": {"$search": name}}))
+            by_name = list(
+                mongo.db.restaurants.find({
+                    "$text": {
+                        "$search": name
+                    }
+                })
+            )
             for restaurant in by_name:
                 if restaurant["address_county"] == county:
                     all.append(restaurant)
@@ -292,7 +417,12 @@ def search():
     else:
         all = mongo.db.restaurants.find().sort("name", 1)
     restaurants = only_approved(all)
-    return render_template("get_restaurants.html", restaurants=restaurants, user=user, counties=counties)
+    return render_template(
+        "get_restaurants.html",
+        restaurants=restaurants,
+        user=user,
+        counties=counties
+    )
 
 
 def only_approved(all):
@@ -314,7 +444,7 @@ def not_found(e):
 def calculate_average_star_score(name):
     restaurant = mongo.db.restaurants.find_one({"name": name})
     reviews = restaurant["reviews"]
-    if len(reviews): 
+    if len(reviews):
         star_scores = []
         for review in reviews:
             star_scores.append(int(review["star_score"]))
